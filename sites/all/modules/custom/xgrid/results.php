2830b844d1485a4f2de4132ef6bdefdde272b96f
Gregory Davis <gdavis2@nd.edu>
2013-04-22 11:23:16 -0400

    Minor fixes to make results.php more robust.
    Signed-off-by: Gregory Davis <gdavis2@nd.edu>

--- sites/all/modules/custom/xgrid/results.php
+++ sites/all/modules/custom/xgrid/results.php
@@ -1,34 +1,41 @@
 <?php
-$id=$_POST["id"];
 
-// also allow gets for now
-if(!$id)
-  $id=$_GET["id"];
+$id = '';
+if (isset($_POST['id'])) {
+	$id = $_POST['id'];
+} else if (isset($_GET['id'])) {
+	$id = $_GET['id'];
+}
 
-  header("Content-type:text/plain");
+header("Content-type:text/plain");
 
-if(!$id){
-  echo "No job ID provided.";
-}else{
-  $xgridUrl="http://jobs.vectorbase.org/xgrid.wsdl";
-  $client = new SoapClient($xgridUrl, array('compression' => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP));
-  $statusOutput = $client->getStatus($id);
-  preg_match("#jobStatus = (.*?);#",$statusOutput,$match);
-  $status=$match[1];
+if (empty($id)) {
+	echo "No job ID provided.";
+} else {
+	$xgridUrl="http://jobs.vectorbase.org/xgrid.wsdl";
+	$client = new SoapClient($xgridUrl, array('compression' => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP));
+	$statusOutput = $client->getStatus($id);
+	preg_match("#jobStatus = (.*?);#",$statusOutput,$match);
+	
+	$status = '';
+	if (!empty($match)) {
+		$status = $match[1];
+	}
 
-  if (!$status)
-    echo "No data for job $id";
-  else if ($status=="Finished")
-    $results = $client->getResults($id);
-  else if ( $status=="Failed" || $status=="Canceled")
-    $results = "Problem with job $id: $status";
-  else if ($status=="Started" || $status=="Running")
-    $results = "Job $id is still running";
-  else if ($status=="Pending")
-    $results = "Job $id is waiting in the queue";
-  else
-    $results = "Job $id status: $status";
-
-  echo $results;
+	$results = '';
+	if (empty($status)) {
+		$results = "No data for job $id";
+	} else if ($status=="Finished") {
+		$results = $client->getResults($id);
+	} else if ($status=="Failed" || $status=="Canceled") {
+		$results = "Problem with job $id: $status";
+	} else if ($status=="Started" || $status=="Running") {
+		$results = "Job $id is still running";
+	} else if ($status=="Pending") {
+		$results = "Job $id is waiting in the queue";
+	} else {
+		$results = "Job $id status: $status";
+	}
+	echo $results;
 }
 
