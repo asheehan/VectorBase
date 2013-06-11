3b0702ed6a68562b065504267a67781b5a4b651a
Dave Campbell <dcamp3@gmail.com>
2013-06-11 09:18:12 -0400

    VB-1957 New popbio js and css from Bob for Pre

--- popbio_static/js/config.js
+++ popbio_static/js/config.js
@@ -1,9 +1,14 @@
 /* no trailing slashes (especially for REST url) */
 
 configTxt = {
-	'REST':'/popbio/REST/',
-  'ROOT':'/',
-'ROOT_STATIC':'/popbio_static/'
+    'REST':'/popbio/REST/',
+    'ROOT':'/popbio/',
+    'ROOT_STATIC':'/popbio_static/',
+    'linkouts' : {
+	'MR4 accession' : 'http://www.mr4.org/MR4ReagentsSearch/Results.aspx?BEINum=####&Template=livingMosquitoes',
+	'BioSamples accession' : 'http://www.ebi.ac.uk/biosamples/sample/####',
+        'UCDavis ID' : 'https://grassi2.ucdavis.edu/PopulationData/DataViews/indiv.php?id=####'
+    }
 };
 
 var config = eval(configTxt);
